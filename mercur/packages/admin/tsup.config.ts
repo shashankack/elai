f0e7676b63bcd80cb8dc952ea363d792df976d6d
shashankack <shashank.ac.dev@gmail.com>
2026-06-18 17:53:08 +0530

    Enhance package.json scripts for setup and add build scripts for admin and vendor apps; update tsup.config to conditionally set dts option.

--- mercur/packages/admin/tsup.config.ts
+++ mercur/packages/admin/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   clean: true,
-  dts: true,
+  dts: process.env.TSUP_DTS !== "0",
   entry: ["src/index.ts", "src/index.css", 'src/pages/index.ts'],
   format: ["esm"],
   external: ["react", "react-dom", "virtual:mercur/config", "virtual:mercur/routes", "virtual:mercur/components", "virtual:mercur/menu-items", "virtual:mercur/i18n"],
